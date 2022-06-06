f38e9bbb5e010a9976d443e47e82d16d202880bb
unknown <a.abdelazizg@gmail.com>
2022-06-06 04:21:09 +0200

    upload project

---
+++ data.js
@@ -0,0 +1,15 @@
+const Rules = [
+    "1. You will have <span class='seconds'>30 seconds",
+    "2. Once you select your answer, you can't reselect.",
+    "3. You can't select any option once time goes off.",
+    "4. You can't exit from the Quiz while you're playing.",
+    "5. You'll get points on the basis of your correct answers."
+];
+
+const Q_A = [
+    { Question : "Question one" , Answers : [ "one", "two", "three", "four" ] },
+    { Question : "Question two" , Answers : [ "one", "two", "three", "four" ] },
+    { Question : "Question three" , Answers : [ "one", "two", "three", "four" ] },
+    { Question : "Question four" , Answers : [ "one", "two", "three", "four" ] },
+    { Question : "Question five" , Answers : [ "one", "two", "three", "four" ] },
+];
